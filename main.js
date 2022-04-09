a9405a94e33450d26add510a29a514afdd80918c
yash <yashgouravkar@gmail.com>
2022-04-09 09:55:13 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const meterToFeet = (meter) => {
+  return meter * 3.28084;
+};
+
+const feetToMeter = (feet) => {
+  return feet * 0.3048;
+};
+
+const litresToGall = (lt) => {
+  return lt * 0.2199692;
+};
+
+const gallToLitres = (gall) => {
+  return gall * 4.54609;
+};
+
+const kgToPound = (kg) => {
+  return kg * 2.204623;
+};
+
+const poundToKg = (pound) => {
+  return pound * 0.4535924;
+};
+
+const input = document.getElementById("data");
+console.log(input);
